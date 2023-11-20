30d793c011016c18204e100177fe7b052462942a
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-20 10:58:53 +0000

    Reordered

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const now = new Date();
 
 const startTimer = document.getElementById('start-button');
 
-function logTime(element) {
-    const elementID = element.id;
-    const currentDateTime = now.toISOString()
-    console.log(`Element ID: ${elementID} DateTime: ${currentDateTime}`)
-}
-
 startTimer.addEventListener('click', (event) => {
     const buttonElement = event.target;
     logTime(buttonElement)
 });
 
+function logTime(calledElement) {
+    const elementID = calledElement.id;
+    const currentDateTime = now.toISOString()
+    console.log(`Element ID: ${elementID} DateTime: ${currentDateTime}`)
+}
