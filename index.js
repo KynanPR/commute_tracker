24b446072a8f67984a22e7825fc309f793e83261
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-20 12:30:06 +0000

    Fix?

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const now = new Date();
 
 const startTimer = document.getElementById('start-button');
 
-startTimer.addEventListener('click', (event) => {
-    const buttonElement = event.target;
-    logTime(buttonElement)
-});
-
 function logTime(calledElement) {
     const elementID = calledElement.id;
     const currentDateTime = now.toISOString()
     console.log(`Element ID: ${elementID} DateTime: ${currentDateTime}`)
 }
+
+startTimer.addEventListener('click', (event) => {
+    const buttonElement = event.target;
+    logTime(buttonElement);
+});
