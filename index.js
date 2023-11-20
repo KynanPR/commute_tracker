c933692a6c5152fa8e0ad603ae681afe4f985f0a
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-20 10:20:34 +0000

    Fixed getDate & getTime fuction calls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello World!")
 const currentDate = new Date()
 
 function logTime() {
-    let date = currentDate.getDate
-    let time = currentDate.getTime
+    let date = currentDate.getDate()
+    let time = currentDate.getTime()
     console.log(date + " - " + time)
 }
