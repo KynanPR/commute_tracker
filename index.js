5cdb3425046104f921d6a8007115dcb6ee7c5c7c
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-20 14:50:48 +0000

    Journey and Commute classes

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 console.log("Hello World!");
 
 const now = new Date();
+const commuteName = 'My Commute'
+const commuteStops = ['outLeftHome', 'outTramStop', 'outOnTram', 'outOffTram', 'outArrivedWork'];
+
+class Journey {
+    constructor(stops) {
+        stops.forEach(element => {
+            this[element] = null;
+        });
+    }
+}
+
+class Commute {
+    constructor(name, stops) {
+        this.name = name;
+        this.stopNames = stops;
+        this.journeys = [];
+    }
+    createJourney() {
+        const newJourney = new Journey(this.stopNames);
+        this.journeys.push(newJourney);
+    }
+}
+
+// let myCommute = {
+//     outLeftHome: 0,
+//     outTramStop: 0,
+//     outOnTram: 0,
+//     outOffTram: 0,
+//     outArrivedWork: 0
+// }
 
 const startTimer = document.getElementById('start-button');
 
@@ -8,6 +38,7 @@ function logTime(calledElement) {
     const elementID = calledElement.id;
     const currentDateTime = now.toISOString()
     console.log(`Element ID: ${elementID} DateTime: ${currentDateTime}`)
+    let
 }
 
 startTimer.addEventListener('click', (event) => {
