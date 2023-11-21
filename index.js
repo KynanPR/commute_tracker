610a4a90ecf1c3e933d70cc9dfafe9ba6e8e8117
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-21 12:27:58 +0000

    include moment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 moment().format(); 
 
 console.log("Hello World!");
@@ -29,8 +31,9 @@ class Journey { // A specific instance as part of a Commute with given Locations
     constructor(locations) { // locations will be an array of Location objects
         // TODO check there are at least 2 Locations and they are destinations
         // DOCS - Can only have 1 journey per day
-        this.date = 
+        this.date = moment().format('YYYY-MM-DD'); // Serves as an identifier for the journey
         this.stops = []; // Init array of stops
+
         let stopIndex = 0;
         locations.forEach(location => { // Create and init a stop for each location
             stopFromLocation = new Stop(location); // Create new Stop from the Location object
@@ -38,6 +41,7 @@ class Journey { // A specific instance as part of a Commute with given Locations
             this.stops.push(stopFromLocation); // Add that Stop to the array
         });
     }
+    // TODO incrementStop method
 }
 
 class Commute { // The top level object with the invariant properties of a commute. eg. Home/Work Locations, Name of commute, Locations available for Journies
@@ -47,21 +51,21 @@ class Commute { // The top level object with the invariant properties of a commu
         this.journeys = [];
     }
     newJourney() {
-        now = new Date()
+        now = new Date() // TODO moment
         const journeyDate =  now.toISOString().substring(0, 10); // TODO moment
         const newJourney = new Journey(this.stopNames);
         this.journeys[journeyDate] = newJourney;
     }
-    incrementStop(journeyDate) {
-        now = new Date()// TODO moment
+    incrementStop(journeyDate) { // TODO move to Journey
+        now = new Date();// TODO moment
         const startState = this.journeys[journeyDate];
         const nextStopIndex = Object.values(startState).indexOf(null);
         const nextStopName = Object.keys(startState)[nextStopIndex];
 
         this.journeys[journeyDate][nextStopName] = now.ge //TODO moment
-
-
     }
+    // TODO addLocation method
+    //
 }
 
 const startTimer = document.getElementById('start-button');
