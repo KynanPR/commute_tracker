b350066e2b38d1a9d8ad20e245a18cac3c2cd674
KynanPR <55924114+KynanPR@users.noreply.github.com>
2023-11-21 12:10:38 +0000

    Remove moment rework classes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import moment from 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js'; // time library
 moment().format(); 
 
 console.log("Hello World!");
@@ -6,33 +5,60 @@ console.log("Hello World!");
 const commuteName = 'My Commute';
 const commuteStops = ['outLeftHome', 'outTramStop', 'outOnTram', 'outOffTram', 'outArrivedWork'];
 
-class Journey {
-    constructor(stops) {
-        stops.forEach(element => {
-            this[element] = null;
+class Location { // a location is a point along the commute journey that will be used to create a stop
+    constructor(name, boolDestination) {
+        this.stopName = name; // User given name of stop. TODO add checks for duplicate stop names
+        this.boolDestination = boolDestination; // boolDestination flags a location as a beginning/ending location, ie. Home or Work     
+    }
+}
+
+class Stop { // Created as part of a journey init, takes a specified location object and creates an instance of it with the timestamp for that journey
+    constructor(location) {
+        this.location = location; // In case the parent Location object needs to be accessed later
+        this.stopIndex = null; // For tracking which number this stop is in a journey. Init to null, set by the setStopNo method, called by Journey init
+        this.name = location.StopName;
+        this.arrivedAt = null; // Initially null so it can be set during the journey
+    }
+    setStopIndex(stopNo) {
+        this.stopIndex = stopNo;
+    }
+    // TODO set arrivedAt method
+}
+
+class Journey { // A specific instance as part of a Commute with given Locations that can vary depending on journey type eg. Tram vs Cycling vs Driving
+    constructor(locations) { // locations will be an array of Location objects
+        // TODO check there are at least 2 Locations and they are destinations
+        // DOCS - Can only have 1 journey per day
+        this.date = 
+        this.stops = []; // Init array of stops
+        let stopIndex = 0;
+        locations.forEach(location => { // Create and init a stop for each location
+            stopFromLocation = new Stop(location); // Create new Stop from the Location object
+            stopFromLocation.setStopIndex(stopIndex);
+            this.stops.push(stopFromLocation); // Add that Stop to the array
         });
     }
 }
 
-class Commute {
-    constructor(name, stops) {
+class Commute { // The top level object with the invariant properties of a commute. eg. Home/Work Locations, Name of commute, Locations available for Journies
+    constructor(name) {
         this.name = name;
-        this.stopNames = stops;
-        this.journeys = {};
+        this.locations = [];
+        this.journeys = [];
     }
     newJourney() {
         now = new Date()
-        const journeyDate =  now.toISOString().substring(0, 10);
+        const journeyDate =  now.toISOString().substring(0, 10); // TODO moment
         const newJourney = new Journey(this.stopNames);
         this.journeys[journeyDate] = newJourney;
     }
     incrementStop(journeyDate) {
-        now = new Date()
+        now = new Date()// TODO moment
         const startState = this.journeys[journeyDate];
         const nextStopIndex = Object.values(startState).indexOf(null);
         const nextStopName = Object.keys(startState)[nextStopIndex];
 
-        this.journeys[journeyDate][nextStopName] = now.ge
+        this.journeys[journeyDate][nextStopName] = now.ge //TODO moment
 
 
     }
@@ -41,8 +67,9 @@ class Commute {
 const startTimer = document.getElementById('start-button');
 
 function logTime(calledElement) {
+    
     const elementID = calledElement.id;
-    const currentDateTime = now.toISOString()
+    const currentDateTime = now.toISOString() // TODO need to change to moment
     console.log(`Element ID: ${elementID} DateTime: ${currentDateTime}`)
 }
 
@@ -50,3 +77,4 @@ startTimer.addEventListener('click', (event) => {
     const buttonElement = event.target;
     logTime(buttonElement);
 });
+
